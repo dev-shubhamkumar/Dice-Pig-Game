dacab54245040f3390513f5672fde70b207668a1
Shubham Kumar <developer.shubham07@gmail.com>
2021-06-17 00:16:12 +0530

    giving Finishing touches

--- script.js
+++ script.js
@@ -14,14 +14,30 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
+
+
+// Starting Conditions
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function() {
     currentScore = 0;
@@ -31,6 +47,7 @@ const switchPlayer = function() {
     player1El.classList.toggle(`player--active`);
 };
 
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -74,3 +91,5 @@ btnHold.addEventListener('click', function() {
         }
     }
 });
+
+btnNew.addEventListener('click', init);
